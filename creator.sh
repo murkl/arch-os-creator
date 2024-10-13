20427bc5cbe8fbd7c00aabe63327b476d63b4a76
murkl <murkl@users.noreply.github.com>
2024-10-13 02:16:47 +0200

    refactoring

--- creator.sh
+++ creator.sh
@@ -1,71 +1,44 @@
-#!/usr/bin/env bash
-set -euo pipefail
+#!/usr/bin/env sh
 
 # /////////////////////////////////////////////////////
 # VARIABLES
 # /////////////////////////////////////////////////////
 
-ARCH_ISO_DOWNLOAD_DIR="$HOME/Downloads"
-ARCH_ISO_DOWNLOAD_URL="https://mirrors.xtom.de/archlinux/iso/latest" # TODO: select mirror
+[ -z "$ARCH_ISO_DOWNLOAD_DIR" ] && ARCH_ISO_DOWNLOAD_DIR="$HOME/Downloads"
+[ -z "$ARCH_ISO_DOWNLOAD_URL" ] && ARCH_ISO_DOWNLOAD_URL="https://mirrors.xtom.de/archlinux/iso/latest"
 
 # /////////////////////////////////////////////////////
-# TUI VARIABLES
+# FUNCTIONS
 # /////////////////////////////////////////////////////
 
-TUI_TITLE="Arch OS USB Creator"
-TUI_WIDTH="80"
-TUI_HEIGHT="20"
+print_white() { printf '%s\n' "$1"; }
+print_green() { printf '\033[32m%s\033[0m\n' "$1"; }
+print_red() { printf '\033[31m%s\033[0m\n' "$1"; }
+print_purple() { printf '\033[35m%s\033[0m\n' "$1"; }
+print_yellow() { printf '\033[33m%s\033[0m\n' "$1"; }
+print_info() { printf '\033[1;37m%s\033[0m\n' "$1"; }
+print_title() { print_purple "// ${1}"; }
 
 # /////////////////////////////////////////////////////
-# PRINT FUNCTIONS
+# MAIN
 # /////////////////////////////////////////////////////
 
-print_green() { echo -e "\e[32m${1}\e[0m"; }
-print_red() { echo -e "\e[31m${1}\e[0m"; }
-print_purple() { echo -e "\e[35m${1}\e[0m"; }
+clear # Clear the screen & print welcome message
+echo && print_title "Welcome to Arch OS Bootable Device Creator"
+print_white "This script downloads the latest Arch Linux ISO"
+print_white "and creates a bootable USB device."
+echo && print_info "Download Dir: ${ARCH_ISO_DOWNLOAD_DIR}"
 
-print_title() {
-    for ((i = ${#1}; i < 62; i++)); do local spaces="${spaces:-} "; done
-    echo -e ""
-    print_purple "┌───────────────────────────────────────────────────────────────────┐"
-    print_purple "│ > ${1} ${spaces} │"
-    print_purple "└───────────────────────────────────────────────────────────────────┘"
-}
-
-# /////////////////////////////////////////////////////
-# OPEN USB CREATOR
-# /////////////////////////////////////////////////////
-
-# Print welcome screen
-whiptail --title "$TUI_TITLE" --msgbox "Welcome to ${TUI_TITLE}!\n\nThis script will download the latest Arch Linux ISO and will create a bootable USB Device." "$TUI_HEIGHT" "$TUI_WIDTH"
-
-# Choose Disk
-disk_array=()
-while read -r disk_line; do
-    disk_array+=("/dev/$disk_line")
-    disk_array+=(" ($(lsblk -d -n -o SIZE /dev/"$disk_line"))")
-done < <(lsblk -I 8 -d -o KNAME -n)
-
-if [ ${#disk_array[@]} == 0 ]; then
-    whiptail --title "$TUI_TITLE" --msgbox "No supported Disk found" $TUI_HEIGHT $TUI_WIDTH
-    exit 1
-fi
-
-usb_disk=$(whiptail --title "$TUI_TITLE" --menu "\nChoose USB Target Device" $TUI_HEIGHT $TUI_WIDTH "${#disk_array[@]}" "${disk_array[@]}" 3>&1 1>&2 2>&3) || exit 1
-
-# Clear screen
-clear
-
-# Download
-arch_latest_version="$(curl -Lfs ${ARCH_ISO_DOWNLOAD_URL}/arch/version)"
+# Fetch download infos
+arch_latest_version="$(curl -Lfs "${ARCH_ISO_DOWNLOAD_URL}"/arch/version)"
 arch_iso_file="archlinux-${arch_latest_version}-x86_64.iso"
-arch_sha_file="sha256sums.txt"
+arch_sha_file="archlinux-${arch_latest_version}-x86_64.sha256"
 
 mkdir -p "$ARCH_ISO_DOWNLOAD_DIR" || exit 1
 
-# Downloading ISO
+# Downloading ISO if not exists
+echo && print_title "Downloading Arch Linux ISO"
 if ! [ -f "${ARCH_ISO_DOWNLOAD_DIR}/${arch_iso_file}" ]; then
-    print_title "Downloading Arch Linux ISO..."
     if ! curl -Lf --progress-bar "${ARCH_ISO_DOWNLOAD_URL}/${arch_iso_file}" -o "${ARCH_ISO_DOWNLOAD_DIR}/${arch_iso_file}.part"; then
         print_red "ERROR: Downloading Arch ISO"
         exit 1
@@ -74,33 +47,84 @@ if ! [ -f "${ARCH_ISO_DOWNLOAD_DIR}/${arch_iso_file}" ]; then
         print_red "ERROR: Moving Arch ISO"
         exit 1
     fi
+    print_green "Finished: ${ARCH_ISO_DOWNLOAD_DIR}/${arch_iso_file} successfuly downloaded"
+else
+    print_white "Skipped: ${ARCH_ISO_DOWNLOAD_DIR}/${arch_iso_file} already exists"
 fi
 
-# Check ISO
-print_title "Check Arch Linux ISO"
-
-# Downloading Checksum
-if ! curl -Lfs "${ARCH_ISO_DOWNLOAD_URL}/${arch_sha_file}" -o "${ARCH_ISO_DOWNLOAD_DIR}/${arch_sha_file}"; then
-    print_red "ERROR: Downloading Checksum"
-    exit 1
+# Downloading Checksum if not exists
+if ! [ -f "${ARCH_ISO_DOWNLOAD_DIR}/${arch_sha_file}" ]; then
+    if ! curl -Lf --progress-bar "${ARCH_ISO_DOWNLOAD_URL}/sha256sums.txt" -o "${ARCH_ISO_DOWNLOAD_DIR}/${arch_sha_file}"; then
+        print_red "ERROR: Downloading Checksum"
+        exit 1
+    else
+        print_green "Finished: ${ARCH_ISO_DOWNLOAD_DIR}/${arch_sha_file} successfuly downloaded"
+    fi
+else
+    print_white "Skipped: ${ARCH_ISO_DOWNLOAD_DIR}/${arch_sha_file} already exists"
 fi
 
+# Change to download dir
 cd "$ARCH_ISO_DOWNLOAD_DIR" || exit 1
+
+# Check ISO sum
+print_white "sha256sum ${ARCH_ISO_DOWNLOAD_URL}/${arch_iso_file}..."
 if grep -qrnw "${arch_sha_file}" -e "$(sha256sum "${arch_iso_file}")"; then
-    rm -f "$ARCH_ISO_DOWNLOAD_DIR/${arch_sha_file}"
-    print_green "Checksum correct"
+    print_green "Checksum is correct"
 else
     print_red "ERROR: Checksum incorrect"
     exit 1
 fi
 
-# Create USB Device
-print_title "Create Bootable USB Device..."
-if ! sudo dd bs=4M if="${arch_iso_file}" of="$usb_disk" status=progress oflag=sync; then
-    print_red "ERROR: Creating USB Device"
+# Choose Disk
+echo && print_title "USB Target Device"
+
+# List disks and number them
+disk_list=$(lsblk -I 8 -d -o KNAME -n)
+counter=1
+printf '%s\n' "$disk_list" | while read -r disk_line; do
+    size=$(lsblk -d -n -o SIZE /dev/"$disk_line")
+    printf '%d) /dev/%s (%s)\n' "$counter" "$disk_line" "$size"
+    counter=$((counter + 1))
+done
+
+# Count total number of disks
+total_disks=$(printf '%s\n' "$disk_list" | wc -l)
+
+# User input
+printf "\nChoose Disk (1-%d): " "$total_disks"
+read -r user_input
+
+# Validate the input
+if ! [ "$user_input" -eq "$user_input" ] 2>/dev/null ||
+    [ "$user_input" -lt 1 ] ||
+    [ "$user_input" -gt "$total_disks" ]; then
+    print_red "Invalid input! Please choose a number between 1 and ${total_disks}"
     exit 1
 fi
 
+# Select the corresponding disk
+selected_disk=$(printf '%s\n' "$disk_list" | sed -n "${user_input}p")
+selected_disk="/dev/$selected_disk"
+print_green "Selected Disk: ${selected_disk}"
+
+# Create USB Device
+echo && print_title "Create Bootable USB Device..."
+printf "CAUTION: All data on %s will be removed! Really proceed? [y/N]: " "$selected_disk"
+read -r confirm
+case "$confirm" in
+[Yy]*) print_info "Proceeding with $selected_disk" ;;
+*) print_red "Operation cancelled" && exit 0 ;;
+esac
+
+# Create bootable device
+print_white "Arch Linux ISO: ${ARCH_ISO_DOWNLOAD_DIR}/${arch_iso_file}"
+if ! sudo dd bs=4M if="${arch_iso_file}" of="$selected_disk" status=progress oflag=sync; then
+    print_red "ERROR: Creating USB Device"
+    exit 1
+fi
+print_green "Bootable USB Device succefully created"
+
 # Finished
-print_title "Finished"
-print_green "Please remove USB Device: $usb_disk"
+echo && print_title "Finished"
+print_yellow "Please remove USB Device or reboot System: $selected_disk"
---
+++ docs/README.md
@@ -0,0 +1,40 @@
+# Arch OS Bootable Device Creator
+
+**[ ➜ One-line POSIX Executable](#run-arch-os-iso-creator)**
+<br><sub></sub>
+
+<div align="center">
+<p><img src="./screenshot.png"></p>
+  <p>
+    <img src="https://img.shields.io/badge/MAINTAINED-YES-green?style=for-the-badge">
+    <img src="https://img.shields.io/badge/License-GPL_v2-blue?style=for-the-badge">
+  </p>
+  <p><sub>100% shellcheck approved</sub></p>
+  <p><sub>powered by <a href="https://github.com/murkl/arch-os">Arch OS</a></sub></p>
+</div>
+
+## Run Bootable Device Creator
+
+This POSIX script downloads the latest [Arch Linux ISO](https://archlinux.org/download/) and creates a bootable USB device.
+
+```
+curl -Ls https://raw.githubusercontent.com/murkl/arch-os-creator/refs/heads/main/creator.sh | bash
+```
+
+Minimal Auto-Installer: Boot from new created USB Device and install **[ ➜ Arch OS](https://github.com/murkl/arch-os)**
+
+## Dependencies
+
+```
+sh curl lsblk
+```
+
+## Override Variables
+
+```
+ARCH_ISO_DOWNLOAD_DIR="~/Downloads"
+ARCH_ISO_DOWNLOAD_URL="https://mirrors.xtom.de/archlinux/iso/latest"
+
+# Example
+ARCH_ISO_DOWNLOAD_DIR="/tmp" ./creator.sh
+```
